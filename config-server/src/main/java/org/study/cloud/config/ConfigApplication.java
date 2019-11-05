bf8d74987b3f5aa99fdfebb06bdf5cb9dcdb4218
lipo <rsq@kaishiba.com>
2019-11-05 19:16:37 +0800

    config

--- config-server/src/main/java/org/study/cloud/config/ConfigApplication.java
+++ config-server/src/main/java/org/study/cloud/config/ConfigApplication.java
@@ -3,6 +3,7 @@ package org.study.cloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-@ComponentScan("org.study.cloud")
+@EnableConfigServer
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
